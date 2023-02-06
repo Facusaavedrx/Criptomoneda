95c03243eabebd21e3de14cbe3ceba7c35272e5a
Facundo Saavedra <facusaavedrx@gmail.com>
2023-02-06 14:46:49 -0300

    Se agrego axion y se pasa cripto desde API al customhook

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import useMoneda from '../hooks/useMoneda'
 import useCriptomoneda from '../hooks/useCriptomoneda'
 import styled from '@emotion/styled'
@@ -22,6 +24,7 @@ const Boton = styled.input`
 `
 
 function Formulario () {
+  const [listacripto, guardarCriptomonedas] = useState([])
   const MONEDAS = [
     { codigo: 'USD', nombre: 'Dolar de Estados unidos' },
     { codigo: 'MXN', nombre: 'Peso Mexicano' },
@@ -29,7 +32,15 @@ function Formulario () {
     { codigo: 'EUR', nombre: 'Euro' }
   ]
   const [moneda, SelectMonedas] = useMoneda('Elige tu moneda', '', MONEDAS)
-  const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '')
+  const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu Criptomoneda', '', listacripto)
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+      const resultado = await axios.get(url)
+      guardarCriptomonedas(resultado.data.Data)
+    }
+    consultarApi()
+  })
   return (
     <form>
       <SelectMonedas />
